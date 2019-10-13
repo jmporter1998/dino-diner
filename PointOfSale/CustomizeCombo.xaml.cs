7a5887e5aaacc24c726f323e1d0d83f3a98e8926
jmporter1998 <jmporter@ksu.edu>
2019-10-13 14:29:02 -0500

    Finished point of sale milestone 1

---
+++ PointOfSale/CustomizeCombo.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PointOfSale
+{
+    /// <summary>
+    /// Interaction logic for CustomizeCombo.xaml
+    /// </summary>
+    public partial class CustomizeCombo : Page
+    {
+        public CustomizeCombo()
+        {
+            InitializeComponent();
+        }
+
+        void SelectDrink(object sender, RoutedEventArgs args)
+        {
+            NavigationService.Navigate(new DrinkSelection());
+        }
+
+        void SelectSide(object sender, RoutedEventArgs args)
+        {
+            NavigationService.Navigate(new SideSelection());
+        }
+    }
+}
